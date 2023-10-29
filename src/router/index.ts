6777cc7b5fef7454d10ca4439eb0c720b8c35eb0
Weda Dewa <weda.dewa@yahoo.co.id>
2023-10-29 10:42:59 +0000

    feat-list-business: Add API and Axios dependencies and implement ListBusiness view component
    - Added "api" and "axios" dependencies to the package.json file
    - Created a new file called "YelpService.ts" in the "services" directory with functions for fetching businesses and business details from the Yelp API
    - Created a new file called "ListBusiness.vue" in the "views" directory with a template for displaying a list of businesses, including search functionality and data table rendering
    - Inside the ListBusiness component, implemented the fetchBusinesses method to fetch businesses from the Yelp API and update the "businesses" data property accordingly
    - Implemented the getBusinessUrl method to generate URLs for business details based on the business ID
    - Implemented the getLocation method to format the business location for display in the data table

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,26 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import ListBusiness from '../views/ListBusiness.vue';
 
 const routes = [
+  // {
+  //   path: '/',
+  //   component: () => import('@/layouts/default/Default.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       name: 'Home',
+  //       // route level code-splitting
+  //       // this generates a separate chunk (about.[hash].js) for this route
+  //       // which is lazy-loaded when the route is visited.
+  //       component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+  //     },
+  //   ],
+  // },
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
+    name: 'ListBusiness',
+    component: ListBusiness,
   },
 ]
 
